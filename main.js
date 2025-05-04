20d6498ae2a66a170e2a018acd7eff6f508fe932
waris <amwaris2005@gmail.com>
2025-05-04 09:05:33 +0530

    Initial portfolio commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// var typed= new typed('#text', {
+    // String:["Frontend Devoloper","Youtuber","Web Devoloper"],
+    // typeSpeed:100,
+    // backSpeed:100,
+    // backDelay:1000,
+    // loop:true
+// });
